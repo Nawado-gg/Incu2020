11084e8bce4107db678337e6eb3d3df3eaac7884
Gleb <glebivanchenko@bayestech.ru>
2020-03-02 20:20:23 +0300

    Homework

--- Automation/server.py
+++ Automation/server.py
@@ -4,9 +4,13 @@ import socket
 
 node = "127.0.0.1"
 
+def hello():
+    device_connection = connect(node)
+    return "Avalible commands: show hostname, show version, set hostname + name"
+
 def connect(node):
     try:
-        device_connection = manager.connect(host = node, port = '1232', username = 'admin', password = 'cisco!123', hostkey_verify = False, device_params={'name':'nexus'})
+        device_connection = manager.connect(host = node, port = '2222', username = 'admin', password = 'cisco!123', hostkey_verify = False, device_params={'name':'nexus'})
         return device_connection
     except:
         print("Unable to connect " + node)
@@ -26,7 +30,7 @@ def getHostname(node):
         hostname = xml_doc.getElementsByTagName("mod:hostname")
         return "Hostname: " + str(hostname[0].firstChild.nodeValue)
     except:
-        'Kek'
+        return 'Can not get hostname'
 
 def getVersion(node):
     device_connection = connect(node)
@@ -39,13 +43,31 @@ def getVersion(node):
                    """
         netconf_output = device_connection.get(('subtree', version))
         xml_doc = xml.dom.minidom.parseString(netconf_output.xml)
-        version = xml_doc.getElementsByTagName("mod:version")
+        version = xml_doc.getElementsByTagName("mod:nxos_ver_str") #also need to check bios_ver_str, loader_ver_str
         return f"Version: {str(version[0].firstChild.nodeValue)}"
     except:
         return "Unable to get this node version"
     
-def setHostname(node):
-    pass
+def setHostname(node, new_hostname):
+    device_connection = connect(node)
+    host_name = '''
+                <config>
+                    <configure xmlns="http://www.cisco.com/nxos:1.0">
+                        <__XML__MODE__exec_configure>
+                            <hostname>
+                                <name>%s</name>
+                            </hostname>
+                        </__XML__MODE__exec_configure>
+                    </configure>
+                </config>    
+                '''
+    config_ = host_name % (new_hostname.split()[-1])
+    try:
+        device_connection.edit_config(target = 'running', config = config_)
+        return (f"New hostname is {new_hostname.split()[-1]}")
+    except:
+        return 'Unable to change the hostname' + \
+                '\nIf you want to change a hostname you need paste "set hostname" + new_hostname as one word'
     
 def Main():
     host = "127.0.0.1"
@@ -54,8 +76,12 @@ def Main():
     mySocket = socket.socket()
     mySocket.bind((host, port))
 
-    mySocket.listen(3)
+    mySocket.listen(2)
     conn, addr = mySocket.accept()
+    
+    #hello_user = hello()
+    #conn.send(hello_user.encode())
+    
     print ("Connection from: " + str(addr))
     while True:
         message = conn.recv(1024).decode()
@@ -63,6 +89,8 @@ def Main():
             message = getHostname(node)
         elif message == 'show version':
             message = getVersion(node)
+        elif 'set hostname' in message:
+            message = setHostname(node, message)
         else:
             message = "I do not understand"
         conn.send(message.encode())
